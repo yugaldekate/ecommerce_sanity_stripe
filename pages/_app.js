e1d0ae2e2afa87ca92aa081968a465e212256e26
Yugal D Dekate <yugaldekate72@gmail.com>
2023-01-23 22:41:07 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import React from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import {Layout} from '../components';
+import '../styles/globals.css';
+
+import { StateContext } from '../context/StateContext';
+import { Toaster } from 'react-hot-toast';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StateContext>
+      <Layout>
+        <Toaster/>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+    
+     
+  ) 
 }
+
+export default MyApp
